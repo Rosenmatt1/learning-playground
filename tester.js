230e4cd7dca8f6db6d02d7e5c8f90608f22b9df0
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-25 14:41:22 -0600

    Added a match.abswhich finds the absoutle difference and Number.EPSILON which finds smallest floating bpoint number greater than 1

--- tester.js
+++ tester.js
@@ -43,8 +43,14 @@ console.log("i++", i)
 console.log(typeof NaN)
 //NaN is technicaly a number 
 
-console.log(0.1 + 0.2)
-console.log(0.1 + 0.2 == 0.3)
+// console.log(Math.abs(0.1 + 0.2))
+// console.log(0.1 + 0.2 == 0.3)
+
+function areTheNumbersAlmostEqual(num1, num2) {
+  return Math.abs(num1 - num2) < Number.EPSILON
+}
+
+console.log(areTheNumbersAlmostEqual(0.1, 0.2, 0.3))
 
 
 
