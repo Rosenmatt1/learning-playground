daea22943d1833b19579587ed3152497a47e3a39
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-24 22:45:01 -0600

    Added a postfix playground incrementor example

--- tester.js
+++ tester.js
@@ -7,9 +7,15 @@ var bar = null;
 
 // console.log((bar !== null) && ((typeof bar === "object") || (typeof bar === "function")))
 
-console.log((bar !== null) && (typeof bar === "object") && (toString.call(bar) !== "[object Array]"));
+// console.log((bar !== null) && (typeof bar === "object") && (toString.call(bar) !== "[object Array]"));
 
 // console.log((bar !== null) && (typeof bar === "object") && (!$.isArray(bar)));
+
+// The postfix increment operator evaluates to the value before it was incremented.
+
+let i = 0
+i++ // 0
+i === 1
 
 
 
