c29aba557bec33cfe760ab809f924eca3c4e1574
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-25 15:47:45 -0600

    Added a recurive palindrone solution

--- tester.js
+++ tester.js
@@ -68,13 +68,13 @@ isInteger = (x) => { return (typeof x === 'number') && (x % 1 === 0) }
 //   console.log(4);
 // })()
 
-palindrone = (str) => {
- let first = ""
- let second = ""
- let reversedSecond = ""
+// palindrone = (str) => {
+//  let first = ""
+//  let second = ""
+//  let reversedSecond = ""
 
-  first = str.substring(0, str.length/2)
-  second = str.substring(str.length / 2, str.length)
+//   first = str.substring(0, str.length/2)
+//   second = str.substring(str.length / 2, str.length)
 
   // for (let i = second.length -1; i >= 0; i--) {
   //   reversedSecond += second[i]
@@ -83,13 +83,40 @@ palindrone = (str) => {
   // console.log("second", first)
   // console.log("reversedSecond", reversedSecond)
 
-  if (first === second) {
-   return true
- }
- return false
-}
+//   if (first === second) {
+//    return true
+//  }
+//  return false
+// }
 
-console.log(palindrone("daddad"))
+// console.log(palindrone("daddad"))
+
+
+function reverseString(str) {
+  if (str === "") // This is the terminal case that will end the recursion
+    return "";
+
+  else
+    return reverseString(str.substr(1)) + str.charAt(0);
+  /* 
+  First Part of the recursion method
+  You need to remember that you won’t have just one call, you’ll have several nested calls
+  Each call: str === "?"        	                  reverseString(str.subst(1))     + str.charAt(0)
+  1st call – reverseString("Hello")   will return   reverseString("ello")           + "h"
+  2nd call – reverseString("ello")    will return   reverseString("llo")            + "e"
+  3rd call – reverseString("llo")     will return   reverseString("lo")             + "l"
+  4th call – reverseString("lo")      will return   reverseString("o")              + "l"
+  5th call – reverseString("o")       will return   reverseString("")               + "o"
+  Second part of the recursion method
+  The method hits the if condition and the most highly nested call returns immediately
+  5th call will return reverseString("") + "o" = "o"
+  4th call will return reverseString("o") + "l" = "o" + "l"
+  3rd call will return reverseString("lo") + "l" = "o" + "l" + "l"
+  2nd call will return reverserString("llo") + "e" = "o" + "l" + "l" + "e"
+  1st call will return reverserString("ello") + "h" = "o" + "l" + "l" + "e" + "h" 
+  */
+}
+console.log(reverseString("hello"))
 
 
 
