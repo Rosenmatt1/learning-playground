61ba4db54433e7fcd5098ca5b12a52b1910a2185
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-04-30 15:08:21 -0600

    Created a compare function the check if th values of an array are the same

--- tester.js
+++ tester.js
@@ -303,7 +303,64 @@ var bar = null;
 // }
 // map([1, 2, 3, 4, 5], n => n * 2) // [2, 4, 6, 8, 10]
 
-const obj = { a: 1, b: 2 }
-const shallowClone = { ...obj }
+// const obj = { a: 1, b: 2 }
+// const shallowClone = { ...obj }
 
-console.log(shallowClone)
+// console.log(shallowClone)
+
+// function isDeepEqual(obj1, obj2, testPrototypes = false) {
+//   if (obj1 === obj2) {
+//     return true
+//   }
+
+//   if (typeof obj1 === "function" && typeof obj2 === "function") {
+//     return obj1.toString() === obj2.toString()
+//   }
+
+//   if (obj1 instanceof Date && obj2 instanceof Date) {
+//     return obj1.getTime() === obj2.getTime()
+//   }
+
+//   if (
+//     Object.prototype.toString.call(obj1) !==
+//     Object.prototype.toString.call(obj2) ||
+//     typeof obj1 !== "object"
+//   ) {
+//     return false
+//   }
+
+//   const prototypesAreEqual = testPrototypes
+//     ? isDeepEqual(
+//       Object.getPrototypeOf(obj1),
+//       Object.getPrototypeOf(obj2),
+//       true
+//     )
+//     : true
+
+//   const obj1Props = Object.getOwnPropertyNames(obj1)
+//   const obj2Props = Object.getOwnPropertyNames(obj2)
+
+//   return (
+//     obj1Props.length === obj2Props.length &&
+//     prototypesAreEqual &&
+//     obj1Props.every(prop => isDeepEqual(obj1[prop], obj2[prop]))
+//   )
+// }
+
+var array1 = [1, 2, 3]
+var array2 = [1, 2, 3]
+
+// console.log(array1 == array2)
+
+compare = (arr1, arr2) => {
+
+  for (let i = 0; i <= arr1.length; i++ ) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(compare(array1, array2))
