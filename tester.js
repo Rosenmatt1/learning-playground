8db34ae981cbf8ff63fae69caa988510cea28ca9
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-12 23:45:26 -0600

    Added an example that demonstrates the the need to bind an object becasue it's not in scope

--- tester.js
+++ tester.js
@@ -858,8 +858,20 @@ function lengths (strings) {
 
 // console.log(concater(array))
 
-const isNaN = x => x !== x
-console.log(isNaN)
+// const isNaN = x => x !== x
+// console.log(isNaN)
+
+var hero = {
+  _name: 'John Doe',
+  getSecretIdentity: function () {
+    return this._name;
+  }
+};
+
+var stoleSecretIdentity = hero.getSecretIdentity;
+
+// console.log(stoleSecretIdentity());
+console.log(hero.getSecretIdentity());
 
 
 
