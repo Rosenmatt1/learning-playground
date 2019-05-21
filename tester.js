b5a1cd14ed695a640e99a181fb967273dd7034a7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-21 13:53:11 -0600

    Added a siple sum function to units in an array

--- tester.js
+++ tester.js
@@ -1043,20 +1043,30 @@ function lengths (strings) {
 
 // console.log(plusMinus(array))
 
+// function catAndMouse(x, y, z) {
+//   let catA = Math.abs(z - x)
+//   let catB = Math.abs(z - y)
 
-function catAndMouse(x, y, z) {
-  let catA = Math.abs(z - x)
-  let catB = Math.abs(z - y)
+//   if (catA > catB) {
+//     return "Cat B"
+//   } else if (catB > catA) {
+//     return "Cat A"
+//   } else {
+//     return "Mouse C"
+//   }
+// }
 
-  if (catA > catB) {
-    return "Cat B"
-  } else if (catB > catA) {
-    return "Cat A"
-  } else {
-    return "Mouse C"
+// // console.log(catAndMouse(1, 2, 3))
+// console.log(catAndMouse(1, 3, 2))
+
+function aVeryBigSum(ar) {
+  let result = 0
+
+  for (let i = 0; i < ar.length; i++) {
+    result += ar[i]
   }
+  return result
 }
 
-// console.log(catAndMouse(1, 2, 3))
-console.log(catAndMouse(1, 3, 2))
+
 
